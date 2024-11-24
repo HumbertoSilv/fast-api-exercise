690d5195cee749434aeb39e9994cabd6eb416bd7
humberto <matheushumberto520@gmail.com>
2024-11-23 22:51:06 -0400

    implementing CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,17 +1,69 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
+# def test_root_deve_retornar_ok_e_ola_mundo():
+#     # arrange
+#     client = TestClient(app)
 
-from fast_api_exercise.app import app
+#     # act
+#     response = client.get('/')
+
+#     # assert
+#     assert response.status_code == HTTPStatus.OK
+#     assert response.json() == {'message': 'hello!'}
 
 
-def test_root_deve_retornar_ok_e_ola_mundo():
-    # arrange
-    client = TestClient(app)
-
+def test_create_user(client):
     # act
-    response = client.get('/')
-
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
     # assert
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'hello!'}
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
