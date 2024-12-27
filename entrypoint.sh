346b3cfee29fa3a8456226eea5e87f98728d204f
humberto <matheushumberto520@gmail.com>
2024-12-27 10:32:10 -0400

    update project

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@
 poetry run alembic upgrade head
 
 # Inicia a aplicação
-poetry run fastapi run fast_api_exercise/app.py
-# poetry run uvicorn --host 0.0.0.0 fast_api_exercise.app:app
+# poetry run fastapi run fast_api_exercise/app.py
+poetry run uvicorn --host 0.0.0.0 --port 8000 fast_zero.app:app
